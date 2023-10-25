912a404ce360ccfe04c0c8cc8979120e08987a16
mehedi1802hasan <mehedi1802hasan@gmail.com>
2023-10-25 14:37:19 +0600

    error handle and posted database

--- src/Pages/EmailPart.jsx
+++ src/Pages/EmailPart.jsx
@@ -5,31 +5,49 @@ import Swal from 'sweetalert2';
 const EmailPart = () => {
     const [email, setEmail] = useState('');
 
-    const handleemail = (e) => {
+    const handleEmail = (e) => {
         e.preventDefault();
         const enteredEmail = email; // Use the email state
-        console.log(enteredEmail);
         setEmail(''); // Clear the email input
-        let timerInterval;
 
-        Swal.fire({
-            title: 'Hello ! welcome to Get global',
-            html: 'Email Saved in the database',
-            timer: 2000,
-            timerProgressBar: true,
-            didOpen: () => {
-                Swal.showLoading();
-                const b = Swal.getHtmlContainer().querySelector('b');
-                timerInterval = setInterval(() => {
-                    b.textContent = Swal.getTimerLeft();
-                }, 100);
+        const addEmail = {
+            email: enteredEmail,
+        };
+
+        fetch('https://get-global-server-dbi6jy4ux-mehedi1802hasan.vercel.app/email', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json',
             },
-            willClose: () => {
-                clearInterval(timerInterval);
-            }
-        }).then((result) => {
-            if (result.dismiss === Swal.DismissReason.timer) {
-                console.log('I was closed by the timer');
+            body: JSON.stringify(addEmail),
+        })
+        .then((res) => res.json())
+        .then((data) => {
+            if (data.insertedId) {
+                let timerInterval;
+                Swal.fire({
+                    title: 'Hello! Welcome to Get Global',
+                    html: 'Email Saved in the database successfully ',
+                    timer: 2000,
+                    timerProgressBar: true,
+                    didOpen: () => {
+                        Swal.showLoading();
+                        const b = Swal.getHtmlContainer().querySelector('b');
+                        if (b) {
+                            b.textContent = Swal.getTimerLeft();
+                            timerInterval = setInterval(() => {
+                                b.textContent = Swal.getTimerLeft();
+                            }, 100);
+                        }
+                    },
+                    willClose: () => {
+                        clearInterval(timerInterval);
+                    }
+                }).then((result) => {
+                    if (result.dismiss === Swal.DismissReason.timer) {
+                        console.log('I was closed by the timer');
+                    }
+                });
             }
         });
     };
@@ -40,8 +58,8 @@ const EmailPart = () => {
                 <h3 className='text-2xl font-bold text-center'>Stay Ahead of the Curve</h3>
                 <p className='text-center mt-3 mb-5'>Get exclusive early access to the finest global job listings, sign <br /> up below and be the first to know when we launch</p>
 
-                <div className='text-center'  >
-                    <form onSubmit={handleemail}> 
+                <div className='text-center'>
+                    <form onSubmit={handleEmail}>
                         <input
                             type="email"
                             name='email'
@@ -51,7 +69,7 @@ const EmailPart = () => {
                             className="input input-bordered input-md md:w-[500px] p-6 rounded-3xl h-[58px]"
                             required
                         />
-                        <button type='submit' className='btn btn-md text-white hover-bg-blue-500 rounded-3xl hover:bg-blue-500 bg-[#645BC8] md:w-[110px] lg::w-[110px] lg:md:-ml-[120px]  md:-ml-[120px]'>Notify me</button>
+                        <button type='submit' className='btn btn-md text-white hover-bg-blue-500 rounded-3xl hover:bg-blue-500 bg-[#645BC8] md:w-[110px] lg::w-[110px] lg:md:-ml-[120px] md:-ml-[120px]'>Notify me</button>
                     </form>
                 </div>
                 <div className='mt-4'>
